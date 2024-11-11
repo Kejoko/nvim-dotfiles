6cc0f6222c2537d5f301bb5972ca5be053f418e4
Kejoko <keeganjkochis@gmail.com>
2024-11-11 16:16:26 -0700

    Shortcut to move between next and previous buffers similarly to how we move everywhere else

--- lua/vim/keymaps.lua
+++ lua/vim/keymaps.lua
@@ -45,3 +45,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Keybindings for moving between buffers as we would do in a normal IDE
+vim.keymap.set("n", "<S-h>", ":bp<CR>")
+vim.keymap.set("n", "<S-l>", ":bn<CR>")
