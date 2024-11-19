e45cf587a3a0d7bcb05682a25fafa2f4fc12b8db
Kejoko <keeganjkochis@gmail.com>
2024-11-18 20:30:49 -0700

    Add button for removing all breakpoints

--- lua/plugins/core/debug/debug.lua
+++ lua/plugins/core/debug/debug.lua
@@ -34,6 +34,7 @@ return {
             { "<F2>", dap.step_over, desc = "Debug: Step Over" },
             { "<F3>", dap.step_out, desc = "Debug: Step Out" },
             { "<leader>b", dap.toggle_breakpoint, desc = "Debug: Toggle Breakpoint" },
+            { "<leader>bc", dap.clear_breakpoints, desc = "Debug: Clear All Brakpoints" },
             {
                 "<leader>B",
                 function()
