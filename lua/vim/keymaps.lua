8a1562a4adc4aafd757ca5595aeda6bb5794ae2f
Kejoko <keeganjkochis@gmail.com>
2024-11-21 22:02:22 -0700

    Close the terminal fully with esc-esc

--- lua/vim/keymaps.lua
+++ lua/vim/keymaps.lua
@@ -26,13 +26,11 @@ vim.keymap.set("n", "<leader>hb", ":bp<bar>sp<bar>bn<bar>bd!<CR>", { desc = "[H]
 -- Open a terminal in the bottom of the current window
 vim.keymap.set("n", "<C-/>", ":split | resize 20 | term <CR> a")
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
+-- Close the terminal
+-- NOTE: This only works in terminal mode
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n> :bd!<CR>", { desc = "Exit terminal mode" })
 
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!"<CR>')
